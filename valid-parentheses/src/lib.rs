0b2cbdf4eff647665df6f7c76a00178940f68f10
Petru <smart__petea@mail.ru>
2021-07-17 23:38:19 +0300

    17/VII.2021

--- valid-parentheses/src/lib.rs
+++ valid-parentheses/src/lib.rs
@@ -2,7 +2,37 @@ struct Solution;
 
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        true
+        let mut stack: Vec<char> = Vec::new();
+        for c in s.chars() {
+            if Solution::is_open_bracket(&c) {
+                stack.push(c);
+                continue;
+            }
+
+            if stack.is_empty() {
+                return false;
+            }
+
+            let open_bracket = Solution::get_open_bracket(&c);
+            if open_bracket != stack.pop() {
+                return false;
+            }
+        }
+
+        stack.is_empty()
+    }
+
+    fn is_open_bracket(c: &char) -> bool {
+         *c == '(' || *c == '{' || *c == '['
+    }
+
+    fn get_open_bracket(close_bracket: &char) -> Option<char> {
+        match close_bracket {
+            ')' => Some('('),
+            '}' => Some('{'),
+            ']' => Some('['),
+            _ => None,
+        }
     }
 }
 
@@ -13,5 +43,10 @@ mod tests {
     #[test]
     fn is_valid() {
         assert_eq!(Solution::is_valid("()".to_string()), true);
+        assert_eq!(Solution::is_valid("()[]{}".to_string()), true);
+        assert_eq!(Solution::is_valid("(]".to_string()), false);
+        assert_eq!(Solution::is_valid("([)]".to_string()), false);
+        assert_eq!(Solution::is_valid("{[]}".to_string()), true);
+        assert_eq!(Solution::is_valid("[".to_string()), false);
     }
 }
